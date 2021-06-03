bb2450729dc7d97ff44a5653dfec79df79435c33
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 23:54:30 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I often go away ....... the weekend.",
+        options:["at", "in", "on"],
+        answer: 0
+    },
+    {
+        q:"I’m starting my new job ....... 7 July.",
+        options:["at", "on", "in"],
+        answer: 1
+    },
+    {
+        q:"We often go to the beach ....... summer.",
+        options:["at", "on", "in"],
+        answer: 2
+    },
+    {
+        q:"George isn’t here ....... the moment.",
+        options:["at", "on", "in" ],
+        answer: 0
+    },
+    {
+        q:"Jane’s birthday is ....... December.",
+        options:["on", "in", "at"],
+        answer: 1
+    },
+    {
+        q:"Do you work ....... Saturdays?",
+        options:["at", "in", "on"],
+        answer: 2
+    },
+    {
+        q:"The company started ....... 1999.",
+        options:["in", "on", "at"],
+        answer: 0   
+    },
+    {
+        q:"I like to look at the stars ....... night.",
+        options:["on", "at", "in"],
+        answer: 1
+    },
+    {
+        q:"I’ll send you the money ....... the end of the month.",
+        options:["on", "in", "at"],
+        answer: 2
+    },
+    {
+        q:"Let's meet ....... half past two?",
+        options:["at", "in", "on"],
+        answer: 0
+    }
+
+]
